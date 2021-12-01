706e521f08e7dcf132118f95b4c0be0cbe6c1c7e
johanne101 <jlopez0493@gmail.com>
2021-12-01 19:14:52 +0000

    [edit] whitespace

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL) 
+	if (new_node == NULL)
 		return (NULL);
 	new_node->parent = parent;
 	new_node->n = value;
