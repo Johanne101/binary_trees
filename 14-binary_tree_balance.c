c907805fd9a341bc2e78f7865a8f50195ac088e8
johanne101 <jlopez0493@gmail.com>
2021-12-02 05:40:00 +0000

    [edit] balance]

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,5 +33,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->right) - binary_tree_height(tree->left));
 }
